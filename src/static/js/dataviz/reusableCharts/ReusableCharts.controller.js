6f636ece17f1ae375e07edbeaff96484e19ea5ab
rnocc <rachel.nocc@gmail.com>
2016-10-21 10:24:53 -0500

    Only load CSV data once

--- src/static/js/dataviz/reusableCharts/ReusableCharts.controller.js
+++ src/static/js/dataviz/reusableCharts/ReusableCharts.controller.js
@@ -5,21 +5,18 @@ angular.module('app')
 .controller('ReusableChartsController', function(libraries){
     let vm = this;
 
-    libraries.d3.csv('./data/treatment_all.csv', function(result) {
-        vm.treatmentBarChartData = result
+    libraries.d3.csv('./data/treatment_all.csv', function(csv) {
+        vm.treatmentBarChartData = csv
         .filter(d => d.Label === 'Tx' && d.Group !== '0 - 21' && d.Race !== 'Total')
         .map(d => {
             d.Value = +d.Value;
             return d;
         });
-    });
 
-    libraries.d3.csv('./data/treatment_all.csv', function(results) {
-        vm.treatmentDonutChartData = results
+        vm.treatmentDonutChartData = csv
         .filter(d => d.Race === 'Total' && d.Label === 'Tx' && d.Region === 'State' && d.Group !== '0 - 21')
         .map(obj => {return {value: obj.Value, type: obj.Group}});
     });
-
 });
 
 })();
